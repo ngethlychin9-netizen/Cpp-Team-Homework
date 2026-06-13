4d94d905982b975c8ed1efcee2bf67f001001541
Ngeth Lychin <ngethlychin9@gmail.com>
2026-06-13 12:55:23 +0700

    Add total to test with bit

---
+++ tesing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /// variable 
+    int a = 0;
+    int b = 100;
+    int c = 30;
+    int total = a +b +c;
+    cout << "Lychin Total: " << total << endl;
+    cout << "hello Bit Sopheaktra" << endl;
+    return 0;
+}
