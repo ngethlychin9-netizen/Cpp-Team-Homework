bc8cac01f76d419906c3d39fe73f75ef7d2f01f6
Ngeth Lychin <ngethlychin9@gmail.com>
2026-06-13 15:46:01 +0700

    Do you see my branch

---
+++ bitTesting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /// variable 
+    int a = 0;
+    int b = 100;
+    int c = 30;
+    int total = a +b +c;
+    cout << "Lychin Total: " << total << endl;
+    cout << "hello Bit Sopheaktra" << endl;
+    cout << "Can you see my branch?" << endl;
+    
+    return 0;
+}
